b479944a7b800b2343dcb0a3cb1f8e1e418014d9
alexover1 <alex@overstreet.me>
2021-05-11 11:06:07 -0400

    Fixed purge route

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,8 @@
 module.exports = {
-  purge: ["./pages/**/*.{js,ts,jsx,tsx}", "./components/**/*.{js,ts,jsx,tsx}"],
+  purge: [
+    "./src/pages/**/*.{js,jsx,ts,tsx}",
+    "./src/modules/**/*.{js,ts,jsx,tsx}",
+  ],
   darkMode: false, // or 'media' or 'class'
   theme: {
     fontFamily: {
